49765aa304b1392229571202b82b44d19920458a
joseteixeiracyber <dev.arturteixeira@gmail.com>
2025-12-22 15:45:46 -0300

    Update login API URL to use HTTP and port 3001

--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -15,7 +15,7 @@ export default function Login() {
         setError("");
 
         try {
-            const response = await fetch("https://apinoples.jtmoney.cloud/auth/login", {
+            const response = await fetch("http://apinoples.jtmoney.cloud:3001/auth/login", {
                 method: "POST",
                 headers: { "Content-Type": "application/json" },
                 body: JSON.stringify({ email, password }),
